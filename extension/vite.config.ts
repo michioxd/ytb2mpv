f14f91b8fc31a42ddf90d6275d756044ec575f7c
michioxd <michio.haiyaku@gmail.com>
2025-04-09 23:51:17 +0700

    adding deps

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // @ts-expect-error why?
+    crx({ manifest }),
+    ViteMinifyPlugin({
+      ignoreCustomComments: []
+    })
+  ],
+  build: {
+    cssCodeSplit: false,
+    minify: "terser",
+    terserOptions: {
+      parse: {
+        html5_comments: false,
+      },
+      format: {
+        comments: false
+      }
+    }
+  }
 })
